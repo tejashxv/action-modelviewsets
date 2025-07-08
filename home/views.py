45106fa0369dd97e6dd9c343cfd4df0b0d0889aa
tejashxv <tejashyadavcr7@gmail.com>
2025-07-08 23:05:41 +0530

    created custome persmission classes

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import *
 from home.serializers import *
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
+from.permission import IsAdminUser
 # Create your views here.
 
 
@@ -29,7 +30,7 @@ class PublicEventViewset(viewsets.ModelViewSet):
     
 class PrivateEventViewset(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     
